10bae12a523340fbf5dcd9b21a33f0a8cc645ce5
azizdevfull <aziz16110904@gmail.com>
2022-11-26 11:41:40 +0500

    added category show

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -47,7 +47,7 @@
                     <tr>
                       <td>{{ $category->id }}</td>
                       <td>{{ $category->title }}</td>
-                      <td><a href="#"><i class="bi bi-eye"></a></i></td>
+                      <td><a href="{{ route('admin.category.show', $category) }}"><i class="bi bi-eye"></a></i></td>
                     </tr>
                     @endforeach
                   </tbody>

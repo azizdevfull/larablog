047229ccd5f19f5133353446606c5ebd046d97d9
azizdevfull <aziz16110904@gmail.com>
2022-11-26 09:34:20 +0500

    split sidebar to other file

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -1,5 +1,16 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <div class="sidebar">
-
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <!-- Add icons to the links using the .nav-icon class
+                 with font-awesome or any other icon font library -->
+            <li class="nav-item">
+              <a href="pages/gallery.html" class="nav-link">
+                <i class="nav-icon far fa-image"></i>
+                <p>
+                  Gallery
+                </p>
+              </a>
+            </li>
+          </ul>
     </div>
   </aside>

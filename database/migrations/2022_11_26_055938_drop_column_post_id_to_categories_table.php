b84c9fcb56e5d7bed9a766bd7512fc62ae1f01c3
azizdevfull <aziz16110904@gmail.com>
2022-11-26 11:04:44 +0500

    drop post_id from categories table

--- database/migrations/2022_11_26_055938_drop_column_post_id_to_categories_table.php
+++ database/migrations/2022_11_26_055938_drop_column_post_id_to_categories_table.php
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('categories', function (Blueprint $table) {
-            $table->unsignedBigInteger('post_id');
+            $table->unsignedBigInteger('post_id')->nullable();
         });
     }
 };

20a5245734b85185a4720521f6dce69859465bcf
azizdevfull <aziz16110904@gmail.com>
2022-11-27 18:54:33 +0500

    add plugin for upload image

--- resources/views/admin/post/create.blade.php
+++ resources/views/admin/post/create.blade.php
@@ -49,10 +49,23 @@
             @enderror
               </div>
               <div class="form-group w-50">
-                <label for="exampleInputFile">Add Image</label>
+                <label for="exampleInputFile">Add Main Image</label>
                 <div class="input-group">
                   <div class="custom-file">
                     <input type="file" class="custom-file-input" name="main_image">
+                    <label class="custom-file-label" >Choose file</label>
+                  </div>
+                  <div class="input-group-append">
+                    <span class="input-group-text">Upload</span>
+                  </div>
+                </div>
+              </div>
+
+              <div class="form-group w-50">
+                <label for="exampleInputFile">Add Image</label>
+                <div class="input-group">
+                  <div class="custom-file">
+                    <input type="file" class="custom-file-input" name="preview_image">
                     <label class="custom-file-label" >Choose file</label>
                   </div>
                   <div class="input-group-append">

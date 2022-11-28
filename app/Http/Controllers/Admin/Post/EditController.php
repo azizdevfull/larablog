44fa72f32027ef19efef75a3e64e23bad57fa642
azizdevfull <aziz16110904@gmail.com>
2022-11-28 14:04:34 +0500

    completed post update in admin dashboard

--- app/Http/Controllers/Admin/Post/EditController.php
+++ app/Http/Controllers/Admin/Post/EditController.php
@@ -2,22 +2,33 @@
 
 namespace App\Http\Controllers\Admin\Post;
 
+use App\Models\Tag;
+use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Admin\Post\PostRequest;
-use App\Models\Post;
+use Illuminate\Support\Facades\Storage;
+use App\Http\Requests\Admin\Post\EditRequest;
 
 class EditController extends Controller
 {
     public function index(Post $post)
     {
-        return view('admin.post.edit', compact('post'));
+        $categories = Category::all();
+        $tags = Tag::all();
+        return view('admin.post.edit', compact('post', 'tags', 'categories'));
     }
 
-    public function update(PostRequest $request, Post $post)
+    public function update(EditRequest $request, Post $post)
     {
         $data = $request->validated();
+
+        $tagIds = $data['tag_ids'];
+        unset($data['tag_ids']);
+        $data['preview_image'] = Storage::disk('public')->put('/images',$data['preview_image']);
+        $data['main_image'] = Storage::disk('public')->put('/images',$data['main_image']);
         $post->update($data);
+        $post->tags()->sync($tagIds);
         return view('admin.post.show', compact('post'));
     }
 }

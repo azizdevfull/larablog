2393c0dd00efc11a79015c283a698cea28959e4c
azizdevfull <aziz16110904@gmail.com>
2022-11-29 09:32:57 +0500

    fix some bugs

--- app/Service/PostService.php
+++ app/Service/PostService.php
@@ -11,12 +11,17 @@ class PostService
     {
         try {
             DB::beginTransaction();
-            $tagId = $data['tag_ids'];
-            unset($data['tag_ids']);
+            if (isset($data['tag_ids'])) {
+                $tagId = $data['tag_ids'];
+                unset($data['tag_ids']);
+            }
+           
             $data['preview_image'] = Storage::disk('public')->put('/images',$data['preview_image']);
             $data['main_image'] = Storage::disk('public')->put('/images',$data['main_image']);
             $post = Post::firstOrCreate($data);
-            $post->tags()->attach($tagId);
+            if (isset($tagId)) {
+                $post->tags()->attach($tagId);
+            }
             DB::commit();
         } catch (\Exception $exception) {
             DB::rollBack();
@@ -26,10 +31,12 @@ class PostService
 
     public function update($data, $post)
     {
-        try {
+        try {            
             DB::beginTransaction();
-            $tagIds = $data['tag_ids'];
-            unset($data['tag_ids']);
+            if (isset($data['tag_ids'])) {
+                $tagIds = $data['tag_ids'];
+                unset($data['tag_ids']);
+            }
             if (isset($data['preview_image'])) {
                 $data['preview_image'] = Storage::disk('public')->put('/images',$data['preview_image']);
             }
@@ -37,7 +44,9 @@ class PostService
                 $data['main_image'] = Storage::disk('public')->put('/images',$data['main_image']);
             }
             $post->update($data);
+            if (isset($tagIds)) {
             $post->tags()->sync($tagIds);
+            }
             DB::commit();
         } catch (\Exception $exception) {
             DB::rollBack();

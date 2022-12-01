3a213a05498d67d1ad69e243afb5cb29a52d5d32
azizdevfull <aziz16110904@gmail.com>
2022-12-01 11:18:25 +0500

    add user store job

--- app/Http/Controllers/Admin/User/StoreController.php
+++ app/Http/Controllers/Admin/User/StoreController.php
@@ -19,9 +19,6 @@ class StoreController extends Controller
     {
         // aaizz@gm.com
         $data = $request->validated();
-        $password = Str::random(10);
-        $data['password'] = Hash::make($password);
-        $user = User::firstOrCreate(['email' => $data['email']],$data);
         Mail::to($data['email'])->send(new PasswordMail($password));
         event(new Registered($user));
         return redirect()->route('admin.user.index');
--- app/Jobs/StoreUserJob.php
+++ app/Jobs/StoreUserJob.php
@@ -2,12 +2,15 @@
 
 namespace App\Jobs;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
 
 class StoreUserJob implements ShouldQueue
 {
@@ -18,7 +21,7 @@ class StoreUserJob implements ShouldQueue
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($data)
     {
         //
     }
@@ -30,6 +33,8 @@ class StoreUserJob implements ShouldQueue
      */
     public function handle()
     {
-        //
+        $password = Str::random(10);
+        $data['password'] = Hash::make($password);
+        $user = User::firstOrCreate(['email' => $data['email']],$data);
     }
 }

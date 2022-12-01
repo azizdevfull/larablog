e1a1c8181eb9cae2487f6294ad32108eeed7dbed
azizdevfull <aziz16110904@gmail.com>
2022-12-01 09:02:41 +0500

    add verify user email

--- app/Http/Controllers/Admin/User/StoreController.php
+++ app/Http/Controllers/Admin/User/StoreController.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\Admin\User\StoreRequest;
 use App\Mail\User\PasswordMail;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Support\Facades\Mail;
 
 class StoreController extends Controller
@@ -22,6 +23,7 @@ class StoreController extends Controller
         $data['password'] = Hash::make($password);
         $user = User::firstOrCreate(['email' => $data['email']],$data);
         Mail::to($data['email'])->send(new PasswordMail($password));
+        event(new Registered($user));
         return redirect()->route('admin.user.index');
     }
 }

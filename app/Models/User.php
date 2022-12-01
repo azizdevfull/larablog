cd1a8b450e63b2293357b5caaf08e3019daa7b54
azizdevfull <aziz16110904@gmail.com>
2022-12-01 09:09:09 +0500

    open notification

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,4 +55,9 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function sendEmailVerificationNotification()
+    {
+        
+    }
 }

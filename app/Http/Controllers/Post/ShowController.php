7f6fbe39280f5d5fa970ff8a21bc2dedd7c84632
azizdevfull <aziz16110904@gmail.com>
2022-12-02 18:21:46 +0500

    completed pos show page

--- app/Http/Controllers/Post/ShowController.php
+++ app/Http/Controllers/Post/ShowController.php
@@ -4,12 +4,19 @@ namespace App\Http\Controllers\Post;
 
 use App\Http\Controllers\Controller;
 use App\Models\Post;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ShowController extends Controller
 {
     public function index(Post $post)
     {
-        return view('post.show', compact('post'));
+        $date = Carbon::parse($post->created_at);
+        $relatedPosts = Post::where('category_id', $post->category_id)
+        ->where('id', '!=', $post->id)
+        ->get()
+        ->take(3);
+        // dd($relatedPosts);
+        return view('post.show', compact('post','date', 'relatedPosts'));
     }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use App\Models\Tag;
+use App\Models\User;
+use App\Models\Comment;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
@@ -26,5 +29,9 @@ class Post extends Model
     public function likedUsers()
     {
         return $this->belongsToMany(User::class, 'post_user_likes', 'post_id','user_id');
+    }
+    public function comments()
+    {
+        return $this->hasMany(Comment::class, 'post_id','id');
     }
 }

c39f5fdcadd4f24f1dea632449788dc21667ed40
azizdevfull <aziz16110904@gmail.com>
2022-12-03 11:04:44 +0500

    completed blog project

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -29,9 +29,6 @@
                             <a class="nav-link" href="{{ route('main.index') }}">Blog</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('category.index') }}">Category</a>
-                        </li>
-                        <li class="nav-item">
                             @auth
                                 
                             <a class="nav-link" href="{{ route('personal.main.index') }}">Personal</a>
